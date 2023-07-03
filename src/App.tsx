5d09db7e8a08c60a5c751a9ada8c0de10e5169cf
Furkan Karabulut <furkan.karabulut@liveoak.bank>
2023-07-02 21:41:52 -0400

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.scss";
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
+import HomeParagraph from "./components/HomeParagraph";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-content">
+        <img src={require("./images/emre-pic.png")} alt="ProfilePicture" />
+
+        <div>
+          <Tabs defaultActiveKey="profile" className="mb-3 tab-style" fill>
+            <Tab eventKey="home" title="Home">
+              <HomeParagraph />
+            </Tab>
+            <Tab eventKey="publications" title="Publications"></Tab>
+            <Tab eventKey="résumé" title="Résumé"></Tab>
+            <Tab eventKey="breakthroughs" title="Breakthroughs"></Tab>
+          </Tabs>
+        </div>
+      </div>
     </div>
   );
 }
