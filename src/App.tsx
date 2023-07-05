82a6433dd213cff531cfbd23f64be14a17a83d62
Furkan Karabulut <furkan.karabulut@liveoak.bank>
2023-07-05 11:01:05 -0400

    col row is used

--- src/App.tsx
+++ src/App.tsx
@@ -9,26 +9,38 @@ import { BsLinkedin } from "react-icons/bs";
 import { SiGooglescholar } from "react-icons/si";
 import { FaGithub } from "react-icons/fa";
 import Image from "react-bootstrap/Image";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
 function App() {
   return (
-    <div className="App">
-      <div className="main-container">
-        <div style={{ display: "flex", flexDirection: "column" }}>
+    <Container style={{ marginTop: "30px" }} className="App" fluid>
+      <Row>
+        <Col
+          xs={2}
+          md={2}
+          lg={2}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "flex-end",
+          }}
+        >
           <Image
             src={require("./images/emre-pic.png")}
             style={{ width: "200px", height: "200px", margin: "25px 100px" }}
           />
           <Image
             src={require("./images/emre-signature.png")}
-            style={{ width: "150px", height: "150px", margin: "25px 100px" }}
+            style={{ width: "150px", height: "150px", margin: "25px 125px" }}
           />
           <SiGmail className="icon-style" />
           <BsLinkedin className="icon-style" />
           <SiGooglescholar className="icon-style" />
           <FaGithub className="icon-style" />
-        </div>
-        <div className="tabs-container">
+        </Col>
+        <Col xs={10} md={10} lg={10} style={{ marginTop: "45px" }}>
           <Tabs defaultActiveKey="profile" className="mb-3 tab-style" fill>
             <Tab eventKey="home" title="Home">
               <HomeParagraph />
@@ -37,9 +49,9 @@ function App() {
             <Tab eventKey="résumé" title="Résumé"></Tab>
             <Tab eventKey="breakthroughs" title="Breakthroughs"></Tab>
           </Tabs>
-        </div>
-      </div>
-    </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
